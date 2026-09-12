e17b80f2e9d82152999de2e7dcf22d3aa9c27364
manas765 <ms7215@srmist.edu.in>
2026-09-13 01:12:43 +0530

    do thisssss babe

--- app/main.py
+++ app/main.py
@@ -10,10 +10,8 @@ app = FastAPI(title="MICROGUARD API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://frontend-peach-kappa-26.vercel.app"
-    ],
+    allow_origins=["http://localhost:5173"],
+    allow_origin_regex=r"https://frontend-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
